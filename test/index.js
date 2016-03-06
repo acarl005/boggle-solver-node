20131071090172cdd139e37255d8712c5eed56cb
Andrew Carlson <acarl005@g.ucla.edu>
2016-03-05 19:32:15 -0800

    add a second test

--- test/index.js
+++ test/index.js
@@ -16,4 +16,19 @@ describe('Boggle solver', () => {
       done();
     });
   });
+
+  it('should solve the board faster because it doesn\'t have to load english again', done => {
+    var ans = [
+      'ARE', 'AGO', 'AGE', 'ERA', 'EGG', 'ERR', 'EGO', 'ERG', 'EVE', 'ERE', 'EAR', 'JOG', 'KEG',
+      'OVA', 'OAK', 'OAR', 'ORE', 'ROE', 'REV', 'RAG', 'AVER', 'EVER', 'EAVE', 'ERGO', 'GROG',
+      'GEAR', 'GAVE', 'GORE', 'OVER', 'OGRE', 'RAVE', 'RAKE', 'REAR', 'ROAR', 'RAGE', 'RARE', 'ROVE',
+      'ERROR', 'EAGER', 'GORGE', 'GROVE', 'GRAVE', 'ROVER', 'ROGER', 'RARER', 'GRAVER', 'OVERAGE'
+    ];
+    var boggle = new Boggle('eorgvregearjkgoe');
+    boggle.solve();
+    boggle.on('solved', words => {
+      expect(words).to.eql(ans);
+      done();
+    });
+  });
 });
